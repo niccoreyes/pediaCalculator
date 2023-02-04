828333c98e6bea94039f640a5fa2ce4a845a3ad4
Thomas Niccolo Reyes <niccoreyes@gmail.com>
2023-02-04 12:06:43 +0800

    Made variable generation easier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:editable/editable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Pedia table'),
+      home: const MyHomePage(title: 'Fluid Balance'),
     );
   }
 }
@@ -32,116 +32,83 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _editableKey = GlobalKey<EditableState>();
+  final _formKey = GlobalKey<FormBuilderState>();
+  Map<String, dynamic> formVal = {
+    'Age': '',
+    'Weight': '',
+    'Temperature': '',
+    'Input': '',
+    'Output': '',
+  };
 
-  List rows = [
-    {
-      "name": 'James LongName Joe',
-      "date": '23/09/2020',
-      "month": 'June',
-      "status": 'completed'
-    },
-    {
-      "name": 'Daniel Paul',
-      "month": 'March',
-      "status": 'new',
-      "date": '12/4/2020',
-    },
-    {
-      "month": 'May',
-      "name": 'Mark Zuckerberg',
-      "date": '09/4/1993',
-      "status": 'expert'
-    },
-    {
-      "name": 'Jack',
-      "status": 'legend',
-      "date": '01/7/1820',
-      "month": 'December',
-    },
-  ];
-  List cols = [
-    {"title": 'Name', 'widthFactor': 0.2, 'key': 'name', 'editable': false},
-    {"title": 'Date', 'widthFactor': 0.2, 'key': 'date'},
-    {"title": 'Month', 'widthFactor': 0.2, 'key': 'month'},
-    {"title": 'Status', 'key': 'status'},
-  ];
-
-  /// Function to add a new row
-  /// Using the global key assigined to Editable widget
-  /// Access the current state of Editable
-  void _addNewRow() {
-    setState(() {
-      _editableKey.currentState?.createRow();
-    });
+  FormBuilderTextField formBuilderText(String label) {
+    return FormBuilderTextField(
+      name: label,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(
+          color: Color(0xFF6200EE),
+        ),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF6200EE)),
+        ),
+      ),
+      onChanged: (val) {
+        setState(() {
+          formVal = _formKey.currentState?.value ?? formVal;
+          if (kDebugMode) {
+            print(formVal.toString());
+          }
+        });
+      },
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.next,
+    );
   }
 
-  ///Print only edited rows.
-  void _printEditedRows() {
-    List editedRows = _editableKey.currentState!.editedRows;
-    print(editedRows);
+  Widget resultsWidget() {
+    return Text(
+        "I/O: ${formVal['Input'] ??= ''}:${formVal['Output'] ??= ''}\nInsensible: ");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 200,
-        leading: TextButton.icon(
-            onPressed: () => _addNewRow(),
-            icon: const Icon(Icons.add),
-            label: const Text(
-              'Add',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            )),
-        title: Text(widget.title),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextButton(
-                onPressed: () => _printEditedRows(),
-                child: const Text('Print Edited Rows',
-                    style: TextStyle(fontWeight: FontWeight.bold))),
-          )
-        ],
-      ),
-      body: Editable(
-        key: _editableKey,
-        columns: cols,
-        rows: rows,
-        zebraStripe: true,
-        stripeColor1: const Color.fromARGB(255, 240, 248, 255),
-        stripeColor2: const Color.fromARGB(255, 240, 240, 240),
-        onRowSaved: (value) {
-          if (kDebugMode) {
-            print(value);
-          }
-        },
-        onSubmitted: (value) {
-          if (kDebugMode) {
-            print(value);
-          }
-        },
-        borderColor: Colors.blueGrey,
-        tdStyle: const TextStyle(fontWeight: FontWeight.bold),
-        trHeight: 80,
-        thStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-        thAlignment: TextAlign.center,
-        thVertAlignment: CrossAxisAlignment.end,
-        thPaddingBottom: 3,
-        showSaveIcon: true,
-        saveIconColor: Colors.black,
-        showCreateButton: true,
-        tdAlignment: TextAlign.left,
-        tdEditableMaxLines: 100, // don't limit and allow data to wrap
-        tdPaddingTop: 14,
-        tdPaddingBottom: 14,
-        tdPaddingLeft: 10,
-        tdPaddingRight: 8,
-        focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.blue),
-            borderRadius: BorderRadius.all(Radius.circular(0))),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: Column(
+                    children: [
+                      FormBuilder(
+                        key: _formKey,
+                        // enabled: false,
+                        onChanged: () {
+                          _formKey.currentState!.save();
+                          debugPrint(_formKey.currentState!.value.toString());
+                        },
+                        autovalidateMode: AutovalidateMode.disabled,
+                        initialValue: formVal,
+                        child: Column(children: [
+                          for (String keys in formVal.keys)
+                            formBuilderText(keys),
+                        ]),
+                      ),
+                      Divider(
+                        height: 26.0,
+                      ),
+                      resultsWidget()
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
